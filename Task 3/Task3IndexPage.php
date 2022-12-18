dd797d1f1b977da3627d1493ec4eacd3871db3b6
Алим <alim_kasenov@bk.ru>
2022-12-18 14:11:34 +0600

    new

--- Task 3/Task3IndexPage.php
+++ Task 3/Task3IndexPage.php
@@ -38,3 +38,49 @@ echo "<ul><li>" . implode("</li><li>", $nameAndAges) . "</li></ul><hr>";
 // здесь ваш код
 $nameAndAges[] = krsort($nameAndAges, $flags = SORT_STRING);
 echo "<ul><li>" . implode("</li><li>", $nameAndAges) . "</li></ul><hr>";
+
+//-------------------------------------------------------------
+
+$jsonString = '{"menu": {
+  "id": "file",
+  "value": "File",
+  "popup": {
+    "menuitem": [
+      {"value": "New", "onclick": "CreateNewDoc()"},
+      {"value": "Open", "onclick": "OpenDoc()"},
+      {"value": "Close", "onclick": "CloseDoc()"}
+    ]
+  }
+}}';
+$jsonArray = [];
+
+// 2. Преобразовать JSON строку в массив и вывести
+$jsonArray = json_decode($jsonString, true);
+
+// вывод массива
+
+echo '<pre>';
+print_r($jsonArray);
+echo '</pre>';
+
+//-------------------------------------------------------------
+
+$integers = [60,43,20,53,7,38,56,7,56,23,13,54,33,39,48,21,12,53,10,20,23,27,4,35,17,55,9,54,52,54,11,1,26,39,35,15,56,59,9,53,20,53,47,42,12,10,23,58,9,54];
+// 3.1 Вывести все числа из массива, между 22 и 35
+foreach ($integers as $arr){
+    if ($arr > 22 && $arr < 35){
+        echo '<pre>';
+        print_r($arr);
+        echo '</pre>';
+    }
+}
+
+// 3.2 Вывести среднеарифметическое значение из массива чисел
+// здесь ваш код
+$sum = 0;
+$count = 0;
+foreach ($integers as $arr){
+    $sum = $sum + $arr;
+    $count++;
+}
+echo ($sum / $count);
